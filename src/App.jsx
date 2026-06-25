6a42a0a73ed3b8edfa2665827ab15fffd89a6a94
Akindepo Toyeeb <akindepotoyeeb222@gmail.com>
2026-06-25 20:58:49 +0100

    feat: built onboarding screen for mobile/desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Onboarding from "./components/Onboarding";
+import Splash from "./components/Splash";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 export default function App() {
   return (
-    <div className="font-semibold text-2xl text-center p-px">
-      Welcome to Ledgr<span className="text-green-500">X</span>
-    </div>
+    <BrowserRouter>
+      <div className="font-semibold text-2xl text-center p-px md:w-100% h-screen md:py-10 md:bg-green-300 md:px-10">
+        {/* Welcome to Ledgr<span className="text-green-500">X</span> */}
+        <Routes>
+          <Route path="/" element={<Splash />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
