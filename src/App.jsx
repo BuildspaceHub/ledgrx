0db8b0f6c723fd9317b34467615a6ca30547d0cc
solomon-israel <solomonisraelayomide630@gmail.com>
2026-06-29 02:59:42 +0100

    feat:created forgot password and reset password screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
 import React from "react";
 import ConfirmEmailPage from "./pages/ConfirmEmailPage";
+import ConfirmPasswordPage from "./pages/ConfirmPasswordPage";
+import CreatPasswordPage from "./pages/CreatPasswordPage";
 
 export default function App() {
   return (
-    <div className="text-center min-h-screen  mx-auto p-4">
-      <ConfirmEmailPage />
+    // <div className="text-center min-h-screen  mx-auto p-4">
+    //   <ConfirmEmailPage />
+    // </div>
+    <div>
+      {/* <ConfirmPasswordPage /> */}
+      <CreatPasswordPage/>
     </div>
   );
 }
