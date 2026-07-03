614e9dbfece3c745ad0f2dd879cad47db927aa5e
jokesoftie <odujoke25@gmail.com>
2026-07-03 13:48:14 +0100

    updated-verified-email-page

--- src/pages/VerifiedEmailPage.jsx
+++ src/pages/VerifiedEmailPage.jsx
@@ -3,17 +3,17 @@ import Button from "../components/Button";
 
 export default function VerifiedEmailPage() {
   return (
-    <div>
+    <div className="min-h-screen p-5">
       {/* image */}
-      <div className="w-30 h-30 mx-auto">
-        <img src="/images/Vector.png" alt="checked button" />
+      <div className="w-30 h-30 mx-auto bg-[#55B7220F] rounded-2xl gap-2 p-5 mt-5">
+        <img src="/images/Group.png" alt="hurray" />
       </div>
       {/* content */}
       <div className="mt-4 flex flex-col mb-6">
-        <h1 className="text-center font-semibold text-[24px] mb-2">
+        <h1 className="text-center font-semibold text-[24px] mb-4">
           Email Verified!
         </h1>
-        <p className="text-[14px]">
+        <p className="text-[14px] text-center line-heigth:1 mb-1">
           Your email has been verified successfully. Your account is now ready
           to use
         </p>
