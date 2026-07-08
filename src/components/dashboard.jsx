4f5fb62f25ed12f64f18188339f71ffdf57f321d
Akindepo Toyeeb <akindepotoyeeb222@gmail.com>
2026-07-08 09:04:04 +0100

    Implementation-of-mobile-bottom-navigation

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -41,8 +41,8 @@ export default function Dashboard() {
   return (
     <div className="bg-gray-100 min-h-full w-full h-full flex flex-col justify-center relative">
       {/* Header */}
-      <div className="w-full h-[42px] flex py-4 mt-50 bg-gray-100 justify-bottom items-center">
-        <div className="w-[357px] flex py-4 mx-auto bg-gray-100 justify-between items-center">
+      <div className="w-full h-[42px] flex py-4 mt-40 bg-gray-100 justify-bottom items-center absolute sticky top-0 z-10">
+        <div className="w-[357px] flex py-4 px-4 mx-auto max-w-[357px] bg-gray-100 justify-between items-center">
           <div className="flex p-1 items-center bg-gray-300 gap-3 rounded-2xl">
             <div className="bg-white justifu-center align-middle w-5 h-5 rounded-full">
               <img src="https://i.pravatar.cc/100" alt="profile" />
@@ -126,14 +126,14 @@ export default function Dashboard() {
       </div>
 
         {/* Bottom Navigation */}
-      <div className="absolute sticky bottom-[0px] bg-gray-200 z-999 w-full">
+      <div className="absolute sticky bottom-[0px] bg-white z-999 w-full">
         <div className="flex justify-between items-center w-full mx-auto h-[72px] pt-[12px] pb-[10px] px-[16px]">
-          <Link className="flex flex-col items-center justify-center text-green-500 w-[65.5px] h-[46px] bg-white rounded">
+          <Link to="/" className="flex flex-col items-center justify-center text-green-500 w-[65.5px] h-[46px] bg-white rounded">
             <TiHome size={22} />
             <span className="text-xs">Home</span>
           </Link>
 
-          <Link className="flex flex-col items-center justify-center text-gray-400 w-[65.5px] h-[46px] bg-white rounded">
+          <Link to="/statistics" className="flex flex-col items-center justify-center text-gray-400 w-[65.5px] h-[46px] bg-white rounded">
             <FaChartBar size={22} />
             <span className="text-xs">Statistics</span>
           </Link>
@@ -143,7 +143,7 @@ export default function Dashboard() {
             <span className="text-xs">History</span>
           </Link>
 
-          <Link className="flex flex-col items-center justify-center text-gray-400 w-[65.5px] h-[46px] bg-white rounded">
+          <Link to="/settings" className="flex flex-col items-center justify-center text-gray-400 w-[65.5px] h-[46px] bg-white rounded">
             <IoSettingsOutline size={22} />
             <span className="text-xs">Settings</span>
           </Link>
