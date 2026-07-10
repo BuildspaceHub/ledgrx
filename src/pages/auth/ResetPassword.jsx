24a98a25ce1643e3bd589e82da9c83eec788e890
cleve-keem <belloharyourmidey@gmail.com>
2026-07-10 16:05:00 +0100

    feat: style the reset password page

--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,84 +1,100 @@
 import React from "react";
 import { BsArrowLeft } from "react-icons/bs";
-import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { resetPasswordSchema } from "../../libs/validators/auth";
+import { HiOutlineLockClosed } from "react-icons/hi";
+import formatErrorMessages from "../../libs/formatters/auth";
 
 export default function ResetPasswordPage() {
   const [password, setPassword] = React.useState("");
   const [confirmPassword, setConfirmPassword] = React.useState("");
-  const [errorMessages, setErrorMessages] = React.useState({});
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showCurrentPassword, setShowCurrentPassword] = React.useState(false);
+  const [error, setError] = React.useState({});
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    const { error, value } = resetPasswordSchema.validate({
+    const { error } = resetPasswordSchema.validate({
       password,
       confirmPassword,
     });
 
     if (error) {
-      // Handle validation errors
-      const errorMessages = error.details.reduce((acc, curr) => {
-        acc[curr.path[0]] = curr.message;
-        return acc;
-      }, {});
-
-      setErrorMessages(errorMessages);
+      const errors = formatErrorMessages(error);
+      console.log(errors);
+      setError(errors);
       return;
     }
-    // Add your password reset logic here
   }
 
   return (
-    <div className="p-4">
+    <div className="min-h-dvh p-4">
+      <BsArrowLeft className="text-[30px] mb-10" />
       <div>
-        <BsArrowLeft className="text-[30px] mb-4" />
         <h1 className="font-bold text-[25px] mb-4">Create A New Password</h1>
         <p className="text-[14px] text-center mb-4">
           Your new password must not be the same as the previous used password.
         </p>
       </div>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div className="">
-          <label htmlFor="password" className="block text-xl">
-            Password
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="password" className="block font-medium mb-1">
+            New Password
           </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="p-2 w-full h-10 rounded-[10px] outline-none bg-[#F0F0F0]"
-          />
-          <FaRegEyeSlash />
+          <div
+            className={`relative flex items-center rounded-[10px] bg-[#D9D9D9] overflow-hidden ${error?.email ? "outline-1 outline-error" : ""} focus:outline-2 focus:outline-brand`}
+          >
+            <HiOutlineLockClosed className="size-6 mx-2 text-brand" />
+            <input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              placeholder="johndoe@example.com"
+              className={`w-full h-12.5 pr-4 pl-1 outline-0`}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              onClick={() => setShowPassword(!showPassword)}
+              type="button"
+              className="absolute right-1 top-1/2 -translate-1/2"
+            >
+              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
+          </div>
+          <span className="text-error text-[12px]">{error?.password}</span>
         </div>
-        {errorMessages.password && (
-          <span className="block mb-1 text-[13px] text-red-500">
-            Password must be 8 characters
-          </span>
-        )}
-
-        <div className="">
-          <label htmlFor="confirmPassword" className="block text-xl">
+        <div>
+          <label htmlFor="password" className="block font-medium mb-1">
             Confirm Password
           </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="p-2 w-full h-10 rounded-[10px] outline-none bg-[#F0F0F0]"
-          />
-          <FaRegEyeSlash />
-        </div>
-        {errorMessages.confirmPassword && (
-          <span className="block mb-1 text-[13px] text-red-500">
-            Both passwords must match.
+          <div
+            className={`relative flex items-center rounded-[10px] bg-[#D9D9D9] overflow-hidden ${error?.confirmPassword ? "outline-1 outline-error" : ""} focus:outline-2 focus:outline-brand`}
+          >
+            <HiOutlineLockClosed className="size-6 mx-2 text-brand" />
+            <input
+              id="password"
+              type="password"
+              value={confirmPassword}
+              placeholder="johndoe@example.com"
+              className={`w-full h-12.5 pr-4 pl-1 outline-0`}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button
+              onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+              type="button"
+              className="absolute right-1 top-1/2 -translate-1/2"
+            >
+              {showCurrentPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
+          </div>
+          <span className="text-error text-[12px]">
+            {error?.confirmPassword}
           </span>
-        )}
+        </div>
 
-        <button className="border bg-[#52C218] text-[#FFFFFF] w-full h-15 p-2 rounded-[10px] ">
+        <button className="border bg-brand text-background w-full h-12.5 p-2 rounded-[10px] ">
           Reset Password
         </button>
       </form>
